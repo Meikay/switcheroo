a035ad04f018374e786faac3972e90607006df22
meisuleen <meikay@github.com>
2019-11-07 17:17:35 -0500

    Update db

--- db/migrate/20191107180958_create_ratings.rb
+++ db/migrate/20191107180958_create_ratings.rb
@@ -2,8 +2,6 @@ class CreateRatings < ActiveRecord::Migration[6.0]
   def change
     create_table :ratings do |t|
       t.integer :stars
-      t.integer :item_id
-      t.integer :user_id
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :item, null: false, foreign_key: true
 
