76dd5f14f1130ea3cf6aff10256e58de774967a7
meisuleen <meikay@github.com>
2019-11-07 14:16:48 -0500

    Add object relationships to models and a few methods to sessions_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+    has_many :ratings
+	has_many :items, through: :cart
+	has_one :cart
+	has_many :ratings, through: :item
+        
     has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
   get '/signup' => 'users#new'
-  get '/login' => 'sessions#new
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   resources :users
   resources :carts
   resources :items
