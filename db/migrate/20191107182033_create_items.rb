bad66f61d87ad277fefcf408cabd336cc79cf127
meisuleen <meikay@github.com>
2019-11-07 17:44:20 -0500

    Create schema

--- db/migrate/20191107182033_create_items.rb
+++ db/migrate/20191107182033_create_items.rb
@@ -3,8 +3,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :item_name
       t.text :item_description
-      t.integer :user_id
-      t.integer :cart_id
       t.belongs_to :cart, null: false, foreign_key: true
 
       t.timestamps
