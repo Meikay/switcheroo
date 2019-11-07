55d4a8e87a67853ea7cc1249b0187772430bfb5a
meisuleen <meikay@github.com>
2019-11-07 13:59:12 -0500

    signup and login routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new
+  resources :users
   resources :carts
   resources :items
   resources :ratings
-  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
