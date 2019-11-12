7b1c977bdad5314a16e704d24c133bf66107f79a
meisuleen <meikay@github.com>
2019-11-11 23:16:44 -0500

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+#User data
+
+15.times do
+    User.create!(
+        username: Faker::Name.unique.name,
+        email: Faker::Internet.free_email, 
+        password: Faker::Internet.password,
+        age: Faker::Number.within(range: 18..40)
+    )
+end    
+
+
+
+#Item data
+15.times do 
+    Item.create!(
+        item_name: Faker::Commerce.product_name,
+        item_description: Faker::Hipster.paragraph,
+        cart_id: Faker::Number.between(from: Cart.first.id, to: Cart.last.id) 
+    )
+end
+
+
+#Rating data
+15.times do    
+     Rating.create!(
+         item_id: Faker::Number.between(from: Item.first.id, to: Item.last.id),
+         user_id: Faker::Number.between(from: User.first.id, to: User.last.id),
+         stars: Faker::Number.within(range: 1..5)
+     )   
+end
+
+#Cart data
+15.times do    
+    Cart.create!(
+        user_id: Faker::Number.between(from: User.first.id, to: User.last.id)
+    )
+end
