031e49b4e15a5558a5092161685030575226d4b4
meisuleen <meikay@github.com>
2019-11-14 10:43:40 -0500

    Fix params in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,10 @@ class SessionsController < ApplicationController
         render :login
     end
 
+    #login
     def create
-        @user = User.find_by(username: params[:user][:email])
-        binding.pry
-        if @user && @user.authenticate(params[:user][:password])
+        @user = User.find_by(email: params[:user][:email])  
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to users_path
         else
