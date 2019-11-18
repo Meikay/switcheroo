b41f09f09ff18a6e9bd850f711987e94a082017b
meisuleen <meikay@github.com>
2019-11-18 13:20:52 -0500

    Add new items and all items links and pages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @item = Item.new
+    end
+
+    def create  #when hit pry, item should have a user_id to know who posted it
+        binding.pry
+    end
 end
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,7 @@
 class RatingsController < ApplicationController
+    # before_action : redirect_if_not_logged_in
+
+    # def new
+    #     @rating = Rating.new
+    # end
 end
