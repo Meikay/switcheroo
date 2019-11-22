1d426268c3732c16bafa6feec08b58265b6a2da6
meisuleen <meikay@github.com>
2019-11-22 10:04:12 -0500

    User is redirected to item#index after adding item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,14 @@
 class ItemsController < ApplicationController
     before_action :redirect_if_not_logged_in
+   
 
     def new
         @item = Item.new
     end
 
-    # def index
-    #     @item = Item.find params[:item_id]
-    # end
-
-    def create  #when hit pry, item should have a user_id to know who posted it
+    def create  
         @item = current_user.items.build(item_params)  
+        #binding.pry
         if @item.save
             redirect_to items_path  #redirect to user's account page with items posted
         else
@@ -24,6 +22,5 @@ class ItemsController < ApplicationController
         params.require(:item).permit(:item_name, :item_description, :user_id)
     end
 
-    #tried adding user_id to item_params
-    #tried creating user before creating item and added user_id to item but still get same error["User must exist"]
+   
 end
